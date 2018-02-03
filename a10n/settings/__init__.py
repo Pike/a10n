e6b0c60f8c42392eb941cde132f91219eb3fdb13
Axel Hecht <axel@pike.org>
2018-02-03 14:06:17 +0100

    bug 1429441, allow configuration to be overwritten by the environment

--- a10n/settings/__init__.py
+++ a10n/settings/__init__.py
@@ -3,7 +3,50 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import absolute_import
+import os
+
+INSTALLED_APPS = ['life', 'pushes', 'mbdb', 'l10nstats']
+
+MAX_HG_RETRIES = 5  # retry HG worker tasks 5 times
+
+# load configs from a local.py file
 try:
-    from .local import *
+    from .local import *  # noqa
 except ImportError:
-    from .base import *
+    pass
+
+# overload configuration from environment, as far as we have it
+try:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['ELMO_DB_NAME'],
+            'USER': os.environ['ELMO_DB_USER'],
+            'PASSWORD': os.environ['ELMO_DB_PASSWORD'],
+            'HOST': os.environ['ELMO_DB_HOST'],
+            'PORT': '',
+            'CONN_MAX_AGE': 500,
+            'OPTIONS': {
+                'charset': 'utf8',
+                'use_unicode': True,
+            },
+            'TEST': {
+                'CHARSET': "utf8",
+                'COLLATION': 'utf8_general_ci',
+            },
+        },
+    }
+except KeyError:
+    pass
+for local_var, env_var in (
+            ('SECRET_KEY', 'ELMO_SECRET_KEY'),
+            ('REPOSITORY_BASE', 'ELMO_REPOSITORY_BASE'),
+            ('TRANSPORT', 'ELMO_TRANSPORT'),
+):
+    if env_var in os.environ:
+        locals()[local_var] = os.environ[env_var]
+
+if 'ELMO_SENTRY_DSN' in os.environ:
+    RAVEN_CONFIG = {
+        'dsn': os.environ['ELMO_SENTRY_DSN']
+    }
--- a10n/settings/base.py
+++
@@ -1,3 +0,0 @@
-INSTALLED_APPS = ['life', 'pushes', 'mbdb', 'l10nstats']
-
-MAX_HG_RETRIES = 5  # retry HG worker tasks 5 times
